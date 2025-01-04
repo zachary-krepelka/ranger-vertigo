98840d940eeea9e2dc84b7534be3eb9853466db1
Zachary Krepelka <155400781+zachary-krepelka@users.noreply.github.com>
2025-01-04 04:51:26 -0500

    substantial refactoring

--- plugin_vertigo.py
+++ plugin_vertigo.py
@@ -3,123 +3,217 @@
 # DATE: Saturday, March 9th, 2024
 # ABOUT: A better way to move vertically in ranger
 # ORIGIN: https://github.com/zachary-krepelka/ranger-vertigo.git
-# UPDATED: Thursday, March 14th, 2024 at 2:05 AM
+# PORT OF: https://github.com/prendradjaja/vim-vertigo.git
+# UPDATED: Saturday, January 4th, 2025 at 4:47 AM
+
 
 from __future__ import (absolute_import, division, print_function)
 from ranger.api.commands import Command
 import ranger.api
 import curses
 
-UP = 'K'
-DOWN = 'J'
-QUIET = False
-HOMEROW = 'asdfghjkl;'
 
-def input():
+UP = 'K'                 #
+DOWN = 'J'               # Change these variables
+QUIET = False            # for customization
+HOMEROW = 'asdfghjkl;'   #
 
-	input = ''
-
-	window = curses.initscr()
-
-	c = window.getch()
-
-	if c == 27:
-		raise Exception()
-	else:
-		input += chr(c)
-
-	if input.isupper():
-
-		return input
-
-	input += window.getkey()
-
-	return input
-
-def translate(letters):
-
-	if len(letters) == 1:
-
-		return HOMEROW.upper().index(letters) + 1
-
-	elif len(letters) == 2:
-
-		helper = lambda letter: HOMEROW.lower().index(letter) + 1
-
-		digit1, digit2 = helper(letters[0]), helper(letters[1])
-
-		if digit2 == 10:
-
-			digit2 = 0
-
-		return digit1 * 10 + digit2
-
-	else:
-
-		raise Exception()
 
 class Vertigo(Command):
 
-	def execute(self):
+    """A command which provides movement via home-row keypresses.
 
-		if self.fm.settings.line_numbers == 'false':
+    This command moves ranger's file selector using a non-conventional input
+    method in the style of an existing Vim plugin.  The command waits for
+    either one or two home-row keypresses to specify the magnitude of the
+    movement, mapping 'asdfghjkl;' to '1234567890'.  The direction of the
+    movement is specified as an input argument.
 
-			self.fm.notify(
-				"Please enable line numbering "
-				"to use the vertigo plugin.",
-				bad = True
-			)
+    Note:
+        This command is not indented to be invoked directly; instead, it
+        should be mapped to a key binding.  This is handled later on using
+        ranger's initialization hook.  See the bottom of the file.
 
-			return
+    Args:
+        self.arg(1): The direction of the movement.
 
-		try:
+            Either 'up', 'down', or 'to'.
 
-			count = translate(input())
+    Input:
+        Either two lowercase, home-row keypresses representing a two-digit
+        number; or one uppercase, home-row keypress representing a
+        single-digit number.  The characters are received after invoking the
+        command.
 
-		except:
+    Examples:
+        Move the file selector up 12 files:
 
-			if not QUIET:
+            :Vertigo up<Enter>as
 
-				self.fm.notify('Vertigo Canceled', bad = True)
+        Move the file selector down 7 files:
 
-			return
+            :Vertigo down<Enter>J
 
-		finally:
+        Move the file selector to the 16th file from the top:
 
-			curses.endwin()
+            :Vertigo to<Enter>ah
 
-		motion = self.arg(1)
+    Messages:
+        The invocation of the command is always accompanied by a resulting
+        message.  These can be silenced by setting QUIET = True.
 
-		relative = self.fm.settings.line_numbers == 'relative'
+            Vertigo {direction} {count}
 
-		if motion == 'up' and relative:
+                This message is issued under normal operation, indicating
+                the direction and magnitude of the moment.
 
-			self.fm.move(up=count)
+            Vertigo Canceled
 
-		elif motion == 'down' and relative:
+                This message is displayed in red if the input is canceled
+                via the escape key.
 
-			self.fm.move(down=count)
+            Vertigo Untranslatable
 
-		else:
+                This message is displayed in red if the input is invalid.
+    """
 
-			motion = 'to'
-			self.fm.move(to=count)
 
-		if not QUIET:
+    @staticmethod
+    def __getkey(window):
 
-			message = 'Vertigo ' + motion  + ' ' + str(count)
+        """Accepts a single-character input.
 
-			self.fm.notify(message)
+        Args:
+            window (obj): A curses window.
+
+        Returns:
+            str: A single-character input.
+
+        Raises:
+            KeyboardInterrupt: If the escape key is pressed.
+        """
+
+        if (key := window.getch()) == 27:
+
+            raise KeyboardInterrupt()
+
+        return chr(key)
+
+
+    @staticmethod
+    def __input():
+
+        """Accepts a one-or-two-character input from the user.
+
+        Returns:
+            str: One input character if uppercase, two characters otherwise.
+
+        Raises:
+            KeyboardInterrupt: If the escape key is pressed.
+        """
+
+        window = curses.initscr()
+
+        if (input := Vertigo.__getkey(window)).isupper(): return input
+
+        input += Vertigo.__getkey(window)
+
+        return input
+
+
+    @staticmethod
+    def __translate(letters):
+
+        """Translates home-row keypresses into numbers.
+
+        Args:
+            letters (str): A sequence of one or two keys from the home-row.
+
+        Returns:
+            int: The home-row number translation.
+
+        Raises:
+            ValueError: If the input string is untranslatable.
+        """
+
+        caseify = lambda text, cond: text.upper() if cond else text.lower()
+        convert = lambda letter, mode: caseify(HOMEROW, mode).index(letter) + 1
+
+        match len(letters):
+
+            case 1: return convert(letters, True)
+
+            case 2:
+
+                digit1, digit2 = [convert(letter, False) for letter in letters]
+
+                return digit1 * 10 + (0 if digit2 == 10 else digit2)
+
+            case _: raise ValueError("faulty length")
+
+
+    def execute(self):
+
+        """Executes the command."""
+
+        if self.fm.settings.line_numbers == 'false':
+
+            self.fm.notify(
+                "Please enable line numbering "
+                "to use the vertigo plugin.",
+                bad = True
+            )
+
+            return
+
+        try: count = self.__translate(self.__input())
+
+        except ValueError:
+
+            if QUIET: return
+            self.fm.notify('Vertigo Untranslatable', bad = True)
+            return
+
+        except KeyboardInterrupt:
+
+            if QUIET: return
+            self.fm.notify('Vertigo Canceled', bad = True)
+            return
+
+        finally: curses.endwin()
+
+        motion = self.arg(1).lower()
+
+        relative = self.fm.settings.line_numbers == 'relative'
+
+        if motion == 'up' and relative:
+
+            self.fm.move(up=count)
+
+        elif motion == 'down' and relative:
+
+            self.fm.move(down=count)
+
+        else:
+
+            motion = 'to'; self.fm.move(to=count)
+
+        if QUIET: return
+
+        self.fm.notify('Vertigo ' + motion.capitalize()  + ' ' + str(count))
+
 
 HOOK_INIT_OLD = ranger.api.hook_init
 
 def hook_init(fm):
 
-	cmd = "map {key} eval fm.execute_console('Vertigo {direction}')"
+    """Maps the Vertigo command to key bindings at startup."""
 
-	fm.execute_console(cmd.format(key=UP, direction='up'))
-	fm.execute_console(cmd.format(key=DOWN, direction='down'))
+    cmd = "map {key} eval fm.execute_console('Vertigo {direction}')"
 
-	HOOK_INIT_OLD(fm)
+    fm.execute_console(cmd.format(key=UP, direction='up'))
+    fm.execute_console(cmd.format(key=DOWN, direction='down'))
+
+    HOOK_INIT_OLD(fm)
 
 ranger.api.hook_init = hook_init
